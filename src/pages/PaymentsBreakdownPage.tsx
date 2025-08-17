ce9c2e2fa68b1fbb37b3959cdec5dad19e3ae43e
Hiroki SAKABE <hiroki.sakabe@icloud.com>
2025-08-17 10:45:54 +0900

    wip

--- src/pages/PaymentsBreakdownPage.tsx
+++ src/pages/PaymentsBreakdownPage.tsx
@@ -1,9 +1,63 @@
 import { usePaymentsBreakdown } from "../data/usePaymentsBreakdown";
 import { Tabs } from "./components/Tabs";
+import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
 
 type Props = {
   fileName: string;
 };
+
+const data01 = [
+  {
+    name: "Group A",
+    value: 400,
+  },
+  {
+    name: "Group B",
+    value: 300,
+  },
+  {
+    name: "Group C",
+    value: 300,
+  },
+  {
+    name: "Group D",
+    value: 200,
+  },
+  {
+    name: "Group E",
+    value: 278,
+  },
+  {
+    name: "Group F",
+    value: 189,
+  },
+];
+const data02 = [
+  {
+    name: "Group A",
+    value: 2400,
+  },
+  {
+    name: "Group B",
+    value: 4567,
+  },
+  {
+    name: "Group C",
+    value: 1398,
+  },
+  {
+    name: "Group D",
+    value: 9800,
+  },
+  {
+    name: "Group E",
+    value: 3908,
+  },
+  {
+    name: "Group F",
+    value: 4800,
+  },
+];
 
 export function PaymentsBreakdownPage({ fileName }: Props) {
   const paymentsBreakdown = usePaymentsBreakdown({ fileName });
@@ -11,6 +65,29 @@ export function PaymentsBreakdownPage({ fileName }: Props) {
   return (
     <>
       <Tabs fileName={fileName} activeTab="breakdown" />
+
+      <PieChart width={730} height={250}>
+        <Pie
+          data={data01}
+          dataKey="value"
+          nameKey="name"
+          cx="50%"
+          cy="50%"
+          outerRadius={50}
+          fill="#8884d8"
+        />
+        <Pie
+          data={data02}
+          dataKey="value"
+          nameKey="name"
+          cx="50%"
+          cy="50%"
+          innerRadius={60}
+          outerRadius={80}
+          fill="#82ca9d"
+          label
+        />
+      </PieChart>
 
       {paymentsBreakdown.status === "loading" ? (
         <div className="mx-auto w-max">

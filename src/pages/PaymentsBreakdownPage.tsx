6dfaf915a5788f71104856fb0f0f56b8e03a690e
Hiroki SAKABE <hiroki.sakabe@icloud.com>
2025-08-17 10:46:45 +0900

    wip

--- src/pages/PaymentsBreakdownPage.tsx
+++ src/pages/PaymentsBreakdownPage.tsx
@@ -32,32 +32,6 @@ const data01 = [
     value: 189,
   },
 ];
-const data02 = [
-  {
-    name: "Group A",
-    value: 2400,
-  },
-  {
-    name: "Group B",
-    value: 4567,
-  },
-  {
-    name: "Group C",
-    value: 1398,
-  },
-  {
-    name: "Group D",
-    value: 9800,
-  },
-  {
-    name: "Group E",
-    value: 3908,
-  },
-  {
-    name: "Group F",
-    value: 4800,
-  },
-];
 
 export function PaymentsBreakdownPage({ fileName }: Props) {
   const paymentsBreakdown = usePaymentsBreakdown({ fileName });
@@ -67,26 +41,7 @@ export function PaymentsBreakdownPage({ fileName }: Props) {
       <Tabs fileName={fileName} activeTab="breakdown" />
 
       <PieChart width={730} height={250}>
-        <Pie
-          data={data01}
-          dataKey="value"
-          nameKey="name"
-          cx="50%"
-          cy="50%"
-          outerRadius={50}
-          fill="#8884d8"
-        />
-        <Pie
-          data={data02}
-          dataKey="value"
-          nameKey="name"
-          cx="50%"
-          cy="50%"
-          innerRadius={60}
-          outerRadius={80}
-          fill="#82ca9d"
-          label
-        />
+        <Pie data={data01} dataKey="value" nameKey="name" fill="#8884d8" />
       </PieChart>
 
       {paymentsBreakdown.status === "loading" ? (

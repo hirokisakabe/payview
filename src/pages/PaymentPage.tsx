e249efadc6e374f6628f608a0f8d0c5c2041db67
Hiroki SAKABE <hiroki.sakabe@icloud.com>
2025-08-17 10:59:46 +0900

    wip

--- src/pages/PaymentPage.tsx
+++ src/pages/PaymentPage.tsx
@@ -1,9 +1,63 @@
 import { usePayments } from "../data/usePayments";
 import { Tabs } from "./components/Tabs";
+import {
+  LineChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Line,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 type Props = {
   fileName: string;
 };
+
+const data = [
+  {
+    "name": "Page A",
+    "uv": 4000,
+    "pv": 2400,
+    "amt": 2400
+  },
+  {
+    "name": "Page B",
+    "uv": 3000,
+    "pv": 1398,
+    "amt": 2210
+  },
+  {
+    "name": "Page C",
+    "uv": 2000,
+    "pv": 9800,
+    "amt": 2290
+  },
+  {
+    "name": "Page D",
+    "uv": 2780,
+    "pv": 3908,
+    "amt": 2000
+  },
+  {
+    "name": "Page E",
+    "uv": 1890,
+    "pv": 4800,
+    "amt": 2181
+  },
+  {
+    "name": "Page F",
+    "uv": 2390,
+    "pv": 3800,
+    "amt": 2500
+  },
+  {
+    "name": "Page G",
+    "uv": 3490,
+    "pv": 4300,
+    "amt": 2100
+  }
+]
 
 export function PaymentPage({ fileName }: Props) {
   const payments = usePayments({ fileName });
@@ -17,26 +71,42 @@ export function PaymentPage({ fileName }: Props) {
           <span className="loading loading-spinner loading-xl"></span>
         </div>
       ) : (
-        <div className="overflow-x-auto">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>利用日</th>
-                <th>項目</th>
-                <th>金額</th>
-              </tr>
-            </thead>
-            <tbody>
-              {payments.payments.map((item, index) => (
-                <tr key={index}>
-                  <td>{item.date}</td>
-                  <td>{item.name}</td>
-                  <td>{item.price.toLocaleString()} 円</td>
+        <>
+          <LineChart
+            width={730}
+            height={250}
+            data={data}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          </LineChart>
+          <div className="overflow-x-auto">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>利用日</th>
+                  <th>項目</th>
+                  <th>金額</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+              </thead>
+              <tbody>
+                {payments.payments.map((item, index) => (
+                  <tr key={index}>
+                    <td>{item.date}</td>
+                    <td>{item.name}</td>
+                    <td>{item.price.toLocaleString()} 円</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </>
       )}
     </>
   );

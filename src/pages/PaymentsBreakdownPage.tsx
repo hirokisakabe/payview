e32fe1c483092b13889ce3056930c3d86a25601a
Hiroki SAKABE <hiroki.sakabe@icloud.com>
2025-08-17 10:55:31 +0900

    wip

--- src/pages/PaymentsBreakdownPage.tsx
+++ src/pages/PaymentsBreakdownPage.tsx
@@ -33,40 +33,65 @@ const data01 = [
   },
 ];
 
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
 export function PaymentsBreakdownPage({ fileName }: Props) {
   const paymentsBreakdown = usePaymentsBreakdown({ fileName });
+  const chartData =
+    paymentsBreakdown.breakdown?.splice(0, 10).map((item) => ({
+      name: item.name,
+      value: item.total,
+    })) || [];
 
   return (
     <>
       <Tabs fileName={fileName} activeTab="breakdown" />
-
-      <PieChart width={730} height={250}>
-        <Pie data={data01} dataKey="value" nameKey="name" fill="#8884d8" />
-      </PieChart>
 
       {paymentsBreakdown.status === "loading" ? (
         <div className="mx-auto w-max">
           <span className="loading loading-spinner loading-xl"></span>
         </div>
       ) : (
-        <div className="overflow-x-auto">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>項目</th>
-                <th>金額</th>
-              </tr>
-            </thead>
-            <tbody>
-              {paymentsBreakdown.breakdown.map((item, index) => (
-                <tr key={index}>
-                  <td>{item.name}</td>
-                  <td>{item.total.toLocaleString()} 円</td>
-                </tr>
+        <>
+          <PieChart width={730} height={250}>
+            <Pie
+              data={chartData}
+              dataKey="value"
+              nameKey="name"
+              innerRadius={60}
+              outerRadius={80}
+              fill="#8884d8"
+              label
+              paddingAngle={5}
+            >
+              {chartData.map((entry, index) => (
+                <Cell
+                  key={`cell-${entry.name}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
               ))}
-            </tbody>
-          </table>
-        </div>
+            </Pie>
+          </PieChart>
+
+          <div className="overflow-x-auto">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>項目</th>
+                  <th>金額</th>
+                </tr>
+              </thead>
+              <tbody>
+                {paymentsBreakdown.breakdown.map((item, index) => (
+                  <tr key={index}>
+                    <td>{item.name}</td>
+                    <td>{item.total.toLocaleString()} 円</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </>
       )}
     </>
   );
